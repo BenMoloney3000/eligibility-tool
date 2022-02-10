742730cbd0844550b36bcd5cd59b26e371c6cb65
Richard Lane <richard@carbon.coop>
2022-02-10 15:53:32 +0000

    Add garbage collector for blank responses

---
+++ prospector/apps/questionnaire/tasks.py
@@ -0,0 +1,17 @@
+from django_rq import job
+
+from . import models
+
+
+def schedule(scheduler):
+    # Run the cleanup every hour)
+    scheduler.cron("30 * * * *", func=cleanup)
+
+
+@job
+def cleanup():
+    """Clean up abandoned and incomplete responses."""
+
+    # Anything that didn't accept the terms can go straight away with no ill effects
+
+    models.Answers.objects.filter(terms_accepted_at__isnull=True).delete()
