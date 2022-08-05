6bad60d4cd688f60b64a804c9c6b5ec79b780702
tomd <tomd+git@aptivate.org>
2022-08-05 18:57:40 +0100

    Configure redis

--- config/settings/platform.py
+++ config/settings/platform.py
@@ -15,6 +15,8 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[])
 
+REDIS_HOST=redis
+
 # Platform.sh allows us to set variables, but they are set in a single
 # environment variable which is a JSON dict.  To make this stable across
 # environments expand those variables into environment variables:
