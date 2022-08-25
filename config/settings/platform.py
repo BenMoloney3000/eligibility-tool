066e67a7d8bdd8fcfa3c8c04cd35da23f120a9e2
tomd <tomd+git@aptivate.org>
2022-08-25 15:03:17 +0100

    Verbose logging

--- config/settings/platform.py
+++ config/settings/platform.py
@@ -45,9 +45,16 @@ if psh_env("LOCALHOST", False):
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
+            "formatter": "verbose",
         },
         "mail_admins": {
             "level": "ERROR",
