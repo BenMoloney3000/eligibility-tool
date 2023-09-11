a4b905e45b3cda6cccf86018184009fe921700a4
Igor Strapko <igor@goodpraxis.coop>
2023-09-11 08:54:56 +0100

    Remove full clean function in Answers model

--- prospector/apps/questionnaire/models.py
+++ prospector/apps/questionnaire/models.py
@@ -456,7 +456,6 @@ class Answers(models.Model):
             self.short_uid = utils.generate_id()  # noqa
             while Answers.objects.filter(short_uid=self.short_uid).exists():
                 self.short_uid = utils.generate_id()  # noqa
-        self.full_clean()
         super().save(*args, **kwargs)
 
     @property
