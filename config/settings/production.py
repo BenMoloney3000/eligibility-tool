eae9d739eee2cbf26b772929c17219bfcfc8f775
BitBodger <bitbodger@proton.me>
2025-07-07 22:05:27 +0100

    Refactor environment variable loading to use Path for .env file

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,6 +3,10 @@ import requests
 from .base import *  # noqa
 from .base import env
 
+from pathlib import Path
+import requests
+
+
 
 # GENERAL
 # ------------------------------------------------------------------------------
@@ -165,5 +169,6 @@ LOGGING = {
 
 # Load environment variables from a .env file if specified
 if env.bool("DJANGO_READ_DOT_ENV_FILE", default=False):
-    env.read_env(str(ROOT_DIR / ".env"))
+    env.read_env(Path(ROOT_DIR) / ".env")
+
 
