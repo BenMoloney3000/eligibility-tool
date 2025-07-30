a31837f7f0f76447a4cf6d7eaea0e6818411e06d
BitBodger <bitbodger@proton.me>
2025-07-30 12:58:50 +0100

    Handle blank numeric values in parity import

--- prospector/apps/parity/management/commands/data_upload.py
+++ prospector/apps/parity/management/commands/data_upload.py
@@ -1,4 +1,5 @@
 import csv
+from decimal import Decimal
 
 from django.core.management.base import BaseCommand
 from django.core.management.base import CommandError
@@ -29,38 +30,38 @@ class Command(BaseCommand):
                     csv_data.address_2 = row[4]
                     csv_data.address_3 = row[5]
                     csv_data.postcode = row[6]
-                    csv_data.sap_score = row[7]
+                    csv_data.sap_score = Decimal(row[7] or 0)
                     csv_data.sap_band = row[8]
-                    csv_data.lodged_epc_score = row[9] or None
+                    csv_data.lodged_epc_score = int(row[9]) if row[9] else None
                     csv_data.lodged_epc_band = row[10] or None
-                    csv_data.tco2_current = row[11]
-                    csv_data.realistic_fuel_bill = row[12]
-                    csv_data.type = row[13]
-                    csv_data.attachment = row[14]
-                    csv_data.construction_years = row[15]
-                    csv_data.heated_rooms = row[16]
-                    csv_data.wall_construction = row[17]
-                    csv_data.wall_insulation = row[18]
-                    csv_data.floor_construction = row[19]
-                    csv_data.floor_insulation = row[20]
-                    csv_data.roof_construction = row[21]
-                    csv_data.roof_insulation = row[22]
-                    csv_data.glazing = row[23]
-                    csv_data.heating = row[24]
-                    csv_data.boiler_efficiency = row[25]
-                    csv_data.main_fuel = row[26]
-                    csv_data.controls_adequacy = row[27]
-                    csv_data.local_authority = row[28]
-                    csv_data.ward = row[29]
-                    csv_data.parliamentary_constituency = row[30]
-                    csv_data.region_name = row[31]
-                    csv_data.tenure = row[32]
-                    csv_data.uprn = row[33] or None
-                    csv_data.lat_coordinate = row[34] or None
-                    csv_data.long_coordinate = row[35] or None
-                    csv_data.lower_super_output_area_code = row[36]
-                    csv_data.multiple_deprivation_index = 0
-                    csv_data.income_decile = 0
+                    csv_data.tco2_current = Decimal(row[12] or 0)
+                    csv_data.realistic_fuel_bill = row[16]
+                    csv_data.type = row[17]
+                    csv_data.attachment = row[18]
+                    csv_data.construction_years = row[19]
+                    csv_data.heated_rooms = int(row[20] or 0)
+                    csv_data.wall_construction = row[22]
+                    csv_data.wall_insulation = row[23]
+                    csv_data.roof_construction = row[24]
+                    csv_data.roof_insulation = row[25]
+                    csv_data.floor_construction = row[26]
+                    csv_data.floor_insulation = row[27]
+                    csv_data.glazing = row[29]
+                    csv_data.heating = row[30]
+                    csv_data.boiler_efficiency = row[31]
+                    csv_data.main_fuel = row[32]
+                    csv_data.controls_adequacy = row[34]
+                    csv_data.local_authority = row[35]
+                    csv_data.ward = row[36]
+                    csv_data.parliamentary_constituency = row[37]
+                    csv_data.region_name = row[38]
+                    csv_data.tenure = row[39]
+                    csv_data.uprn = row[40] or None
+                    csv_data.lat_coordinate = Decimal(row[41]) if row[41] else None
+                    csv_data.long_coordinate = Decimal(row[42]) if row[42] else None
+                    csv_data.lower_super_output_area_code = row[43]
+                    csv_data.multiple_deprivation_index = int(row[47] or 0)
+                    csv_data.income_decile = int(row[46] or 0)
 
                     temp_data.append(csv_data)
 
