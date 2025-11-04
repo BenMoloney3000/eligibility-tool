ef627f56c472b48cb10a872786407d1f0c5c0fc4
Tom Wilson <tom.s.wilson@gmail.com>
2025-11-04 12:33:12 +0000

    Fix issue where income values are not validated as 64 bit ints, and ensure echoed output is properly escaped.

--- prospector/apps/questionnaire/tests/test_utils.py
+++ prospector/apps/questionnaire/tests/test_utils.py
@@ -15,3 +15,11 @@ def test_generate_id():
             numbers.append(item)
     assert len(numbers) == 5
     assert len(letters) == 5
+
+
+def test_is_valid_64_bit_integer():
+    assert utils.is_valid_64_bit_integer(1000) == True
+    assert utils.is_valid_64_bit_integer(utils.MAX_64_BIT_INT) == True
+    assert utils.is_valid_64_bit_integer(utils.MIN_64_BIT_INT) == True
+    assert utils.is_valid_64_bit_integer(utils.MAX_64_BIT_INT + 1) == False
+    assert utils.is_valid_64_bit_integer(utils.MIN_64_BIT_INT - 1) == False
